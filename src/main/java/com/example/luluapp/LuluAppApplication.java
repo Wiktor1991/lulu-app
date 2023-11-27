f32b492edf1d34142f6fce5ff0bd74b94371bb68
elwiktor <lulinskiwiktor@gmail.com>
2023-11-27 18:28:07 +0100

    [add] - first commit

---
+++ src/main/java/com/example/luluapp/LuluAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.luluapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuluAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuluAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/luluapp/LuluAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luluapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuluAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
