54ea4fe863094eec2a0189566cc5f6b5de811a4a
lulin <lulinskiwiktor@gmail.com>
2024-01-04 13:14:38 +0100

    [change] - build-restart file for dock-network modified

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build . -t luluapp
 docker stop luluapp || true
 docker rm luluapp || true
-docker run -d -p 8080:8080 --name=luluapp luluapp
+docker run -d -p 8080:8080 --name=luluapp luluapp --network lulu --restart unless-stopped
